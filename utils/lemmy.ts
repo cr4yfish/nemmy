ebc63552431b11742cd6e9e56029bb66eabf961a
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-06 01:57:31 +0200

    Added util function, dependency of useSession

--- utils/lemmy.ts
+++ utils/lemmy.ts
@@ -1,5 +1,9 @@
-import { LemmyHttp } from "lemmy-js-client"
+import { LemmyHttp, GetSiteResponse } from "lemmy-js-client"
 
+const getUserDetails = async (jwt: string) => {
+    const user: GetSiteResponse = await fetch(`/api/getSite?auth=${jwt}`).then(res => res.json());
+    return user as GetSiteResponse;
+}
 
   
-export {  }
+export { getUserDetails }
