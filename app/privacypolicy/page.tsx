b468c7a19176ffcc5ac64b8050dac8d88b6891ed
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-05 13:48:36 +0200

    Added privacy policy

---
+++ app/privacypolicy/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function PrivacyPolicy() {
+
+
+    return (
+        <>
+        <main className="p-6">
+            <h1 className="font-bold text-3xl">Privacy Policy</h1>
+            <div>
+                I dont collect any personal data on the app. There is Vercel Analytics but it doesnt collect any personal data.
+            </div>
+        </main>
+        </>
+    )
+}
