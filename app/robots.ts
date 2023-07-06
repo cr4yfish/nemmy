4dce3c1f7fe219726a27669e28a6eb733e23f4fa
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-06 20:31:20 +0200

    Updated to domain to current one

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: "*",
             allow: "/",
         },
-        sitemap: "https://nemmy.vercel.app/sitemap.xml",
+        sitemap: "https://nemmy.app/sitemap.xml",
     }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://nemmy.vercel.app",
+            url: "https://nemmy.app",
             lastModified: new Date(),
         }
     ]
