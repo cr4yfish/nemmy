2544316517c9c7e2f31b7425b9c4e3232d111ef1
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-07 20:12:43 +0200

    Added auth to request & waiting for auth

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { CommunityId, ListingType, PostView, SortType } from "lemmy-js-client";
 import InfiniteScroll from "react-infinite-scroller";
+import { useSession } from "@/hooks/auth";
 
 import Post from "./Post";
 
@@ -36,13 +37,14 @@ export default function PostList({ fetchParams={ limit: 10, page: 1 } } : {
             saved_only?: boolean, auth?: string
         }
     }) {
+    const { session } = useSession();
     const [posts, setPosts] = useState<PostView[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(fetchParams.page || 1);
     const [pageLimit, setPageLimit] = useState<number>(fetchParams.limit || 10);
     const [morePages, setMorePages] = useState<boolean>(true);
 
     const getPosts = async ({ page=1 } : { page?: number }) => {
-        const data = await fetch(`/api/getPosts?limit=${pageLimit}&page=${page}&community_name=${fetchParams.community_name}`);
+        const data = await fetch(`/api/getPosts?limit=${pageLimit}&page=${page}&community_name=${fetchParams.community_name}&auth=${session?.jwt}`);
         const json = (await data.json()).posts;
         if(json.length === 0) {
             setMorePages(false);
@@ -52,6 +54,7 @@ export default function PostList({ fetchParams={ limit: 10, page: 1 } } : {
     }
 
     const handleLoadMore = async () => {
+        if(session.pendingAuth) return;
         const data = await getPosts({ page: currentPage });
         setPosts([...posts, ...data]);
         setCurrentPage(currentPage + 1);
