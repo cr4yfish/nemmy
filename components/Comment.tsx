e1c8ff08cdd0b61d808a5e567fb2229596c7825c
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-07 21:48:46 +0200

    Added voting to comments, improved design

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,8 +1,9 @@
 import { CommentView, GetCommentsResponse } from "lemmy-js-client"
 import React, { useState, useEffect } from "react"
 import RenderMarkdown from "./ui/RenderMarkdown"
-
+import Vote from "./Vote"
 import Username from "./User/Username"
+import { useSession } from "@/hooks/auth"
 
 // Tailwind colors for comment chain colors
 const colors = ["bg-red-300", "bg-orange-300", "bg-amber-300", "bg-yellow-300", "bg-lime-300", "bg-green-300", 
@@ -11,7 +12,7 @@ const colors = ["bg-red-300", "bg-orange-300", "bg-amber-300", "bg-yellow-300", 
 import styles from "@/styles/Comment.module.css"
 
 export default function Comment({ commentView, allComments, depth=0 }: { commentView: CommentView, allComments: CommentView[], depth?: number }) {
-
+    const { session } = useSession();
     const [children, setChildren] = useState<CommentView[]>([]);
     const [childrenHidden, setChildrenHidden] = useState(false);
     const [childrenError, setChildrenError] = useState(true);
@@ -19,9 +20,10 @@ export default function Comment({ commentView, allComments, depth=0 }: { comment
     // Load children from api
     useEffect(() => {
         if(!childrenError) return;
+        if(session.pendingAuth) return;
         
         (async () => {
-            const data = await fetch(`/api/getComments?post_id=${commentView.comment.post_id}&parent_id=${commentView.comment.id}&sort=Top&limit=100&page=0&max_depth=1
+            const data = await fetch(`/api/getComments?post_id=${commentView.comment.post_id}&parent_id=${commentView.comment.id}&sort=Top&limit=100&page=0&max_depth=1&auth=${session.jwt}
             `);
             const json = (await data.json());
             if(json.error) {
@@ -61,14 +63,10 @@ export default function Comment({ commentView, allComments, depth=0 }: { comment
                             <RenderMarkdown>{commentView?.comment?.content}</RenderMarkdown>
                         </div>
                         <div className={`${styles.commentInteractions}`}>
-                            <div className={`${styles.commentVotes}`}>
-                                <span className={`material-icons ${styles.upvote}`}>arrow_upward</span>
-                                <span className={`${styles.votesCount}`}>{commentView?.counts.score}</span>
-                                <span className={`material-icons ${styles.downvote}`}>arrow_downward</span>
-                            </div>
-                            <div className={`${styles.commentReply}`}><span className="material-icons">chat_bubble_outline</span>Reply</div>
-                            <div className={`${styles.commentShare}`}><span className="material-icons">share</span></div>
-                            <div className={`${styles.commentMore}`}><span className="material-icons">more_vert</span></div>
+                            <Vote comment={commentView} isComment horizontal />
+                            <div className={`${styles.interaction}`}><span className="material-icons">chat_bubble_outline</span>Reply</div>
+                            <div className={`${styles.interaction}`}><span className="material-icons">share</span></div>
+                            <div className={`${styles.interaction}`}><span className="material-icons">more_vert</span></div>
                         </div>
                     </div>
 
