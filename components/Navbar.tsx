be2afbfbe27ff48db752d39ea150e83e212f3870
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-08 13:42:46 +0200

    Updated Navbar design & Added Usermenu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 import { FormEvent, useState } from "react";
 import Link from "next/link";
@@ -8,9 +9,10 @@ import Username from "./User/Username";
 
 export default function Navbar() {
     const { session } = useSession();
-    const { navbar } = useNavbar();
+    const { navbar, setNavbar } = useNavbar();
     const [isSearching, setIsSearching] = useState(false);
     const [filterClicked, setFilterClicked] = useState(false);
+    const [userMenu, setUserMenu] = useState(false);
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
@@ -25,20 +27,76 @@ export default function Navbar() {
         <div className={className}>{children}</div>
     }
 
+    const handleFilterOverlayClose = () => {
+        setTimeout(() => {
+            setFilterClicked(false);
+            navbar && setNavbar({...navbar, overlayActive: false})
+        }, 50)
+    }
+
+    const handleUserMenuOverlayClose = () => {
+        setTimeout(() => {
+            handleUserMenuClose();
+            navbar && setNavbar({...navbar, overlayActive: false})
+        }, 50)
+    }
+
+    const handleUserMenuClose = () => {
+        // close the menu
+        setUserMenu(false);
+
+        setTimeout(() => {
+            // set display: none
+            document.getElementById("usermenu")?.style.setProperty("display", "none");
+        }, 300)
+    }
+
+    const handleUserMenuOpen = () => {
+        // set display: block
+        document.getElementById("usermenu")?.style.setProperty("display", "flex");
+
+        setTimeout(() => {
+            // open the menu
+            setUserMenu(true);
+        }, 10)
+
+    }
+
     return (
         <>
         <nav className={`${styles.wrapper} ${navbar?.hidden && "hidden"}`}>    
             <a href="/" className={styles.logo}>Nemmy</a>
 
-            { navbar?.showSort &&
-            <div className={`${styles.filter}`}>
-                <button className={`${styles.filterTitle}`} onClick={() => setFilterClicked(!filterClicked)}><span className="material-icons" >filter_list</span><span className={`${styles.filterTitleText}`}>Filter</span> </button> 
-                <div className={`${styles.filterOptions} ${filterClicked && styles.filterActive}`}>
-                    <button ><span className="material-icons">home</span>Home</button>
-                    <button><span className="material-icons">trending_up</span>Trending</button>
-                </div>
+            <div className="flex flex-row gap-4 items-center">
+
+                { navbar?.showMenu &&
+                <button className={`${styles.menu}`}>
+                    <span className="material-icons">menu</span>
+                </button>
+                }
+
+                { navbar?.showFilter &&
+                <button className={`${styles.navButton}`} onClick={() =>{ setFilterClicked(!filterClicked); handleUserMenuClose(); setNavbar({...navbar, overlayActive: !filterClicked})  }}>
+                    <div>
+                        <span className="material-icons">filter_list</span>
+                        <span className={`${styles.navButtonText}`}>All</span>
+                    </div>
+                    <span className="material-icons">arrow_drop_down</span> 
+                </button> 
+                }
+
+                { navbar?.showSort &&
+                <button className={`${styles.navButton}`}>
+                    <div className="flex items-center gap-1">
+                        <span className="material-icons-outlined">sort</span>
+                        <span className={`${styles.navButtonText}`}>Hot</span>
+                    </div>
+                    <span className="material-icons">arrow_drop_down</span> 
+                </button>
+                }
             </div>
-            }
+
+            
 
             { navbar?.showback &&
             <div className={`${styles.backButton}`}>
@@ -46,31 +104,94 @@ export default function Navbar() {
             </div>
             }
 
-            { navbar?.showSearch &&
-            <form onSubmit={(e) => handleSubmit(e)} className={`${styles.searchWrapper} ${isSearching && styles.searchWrapperActive}`}>
-                <span className="material-icons">search</span>
-                <input onFocus={(e) => setIsSearching(true)} onBlur={(e) => e.currentTarget.value.length == 0 && setIsSearching(false)} className={`${styles.search}`} type="text" placeholder="Search" />
-                <a className={`${styles.searchIcon} ${isSearching && styles.searchIconActive}`} target="_blank">
-                    <button type="submit" className={`${styles.submitButton}`}>
-                        <span className="material-icons">arrow_circle_right
-                        </span>
+            <div className="flex flex-row items-center gap-4">
+
+                { navbar?.showSearch &&
+                <form onSubmit={(e) => handleSubmit(e)} className={`${styles.navButton} ${styles.searchWrapper} ${isSearching && styles.searchWrapperActive}`}>
+                    <span className="material-icons">search</span>
+                    
+                    {isSearching && <input onBlur={(e) => e.currentTarget.value.length == 0 && setIsSearching(false)} className={`${styles.search}`} type="text" placeholder="Search" />}
+                </form>
+                }
+
+                
+                { navbar?.showUser &&
+                <>
+                 {session ? 
+                    <button onClick={() => { handleUserMenuOpen(); handleFilterOverlayClose(); setNavbar({ ...navbar, overlayActive: true }) }}  className={`${styles.userWrapper} cursor-pointer select-none`}>
+                        <div className={styles.userImage}><img src={session.user.my_user?.local_user_view.person?.avatar || "https://i.imgur.com/IN6ZY30.png" } alt={"Account"} /></div>
                     </button>
-                </a>
-            </form>
-            }
+                :
+                    <Link href="/auth">
+                        <button className={`${styles.userWrapper} cursor-pointer select-none`}>
+                            Login
+                        </button>
+                    </Link>
+                    
+                }
+                </>
+                }
+
+            </div>
+
+          
 
             
-            { navbar?.showUser &&
-            <AutoLink href={`/u/${session?.user?.my_user?.local_user_view?.person?.name}`} className={`${styles.userWrapper} cursor-pointer select-none`}>
-                {session?.user?.my_user?.local_user_view?.person?.avatar?
-                    <div className={styles.userImage}><img src={session.user.my_user.local_user_view.person.avatar} /></div>
-                : <span className={`material-icons`}>person</span>
-                }
-                <span>{session?.user?.my_user?.local_user_view?.person?.name || <a href="/auth"><button>Login</button></a>}</span>
-            </AutoLink>
-            }
-            
         </nav>
+
+        {/* Mobile Menu Left Side */}
+
+        {/* User Menu Right Side */}
+        <div id="usermenu" className={`${styles.userMenu} ${userMenu && styles.userMenuActive}`}>
+            <div className={`${styles.userMenuTop}`}>
+
+                <div className={`${styles.userProfiles}`}>
+
+                    <div className={`${styles.userProfile}`}>
+                        <img className={`${styles.userProfileBanner}`} src={session!.user.my_user?.local_user_view.person.banner} alt="" />
+                        <div className={`${styles.userProfileBannerOverlay}`}></div>
+                        <img className={`${styles.userProfileAvatar}`} src={session!.user.my_user?.local_user_view.person.avatar} alt="" />
+                        <div className={`${styles.userProfileText}`}>
+                            <span className={`${styles.userProfileUsername}`}>u/{session!.user.my_user?.local_user_view.person.name}</span>
+                            <span className={`${styles.userProfileDisplayName}`}>{session!.user.my_user?.local_user_view.person.display_name}</span>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center items-center w-12 h-52 px-6">
+                        <button onClick={() => alert("Multiple Accounts are work in progress :)")}><span className="material-icons">add_circle</span></button>
+                    </div>
+                </div>
+
+                <div className={`${styles.userMenuInteractionsTop}`}>
+                    <button><span className="material-icons-outlined">notifications</span>Notifications</button>
+                    <Link onClick={() => handleUserMenuOverlayClose()} href={`/u/${session?.user.my_user?.local_user_view?.person?.name}`}><button><span className="material-icons-outlined">account_circle</span>My Profile</button></Link>
+                    <button><span className="material-icons-outlined">add_circle_outline</span>Create a Post</button>
+                    <button><span className="material-icons-outlined">group_add</span>Create a Community</button>
+                    <button><span className="material-icons-outlined">bookmarks</span>Bookmarked</button>
+                </div>
+
+
+            </div>
+
+            <div className={`${styles.userMenuInteractionsBottom}`}>
+                <button onClick={() => handleUserMenuOverlayClose()}><span className="material-icons-outlined">close</span>Close</button>
+                <button><span className="material-icons-outlined">settings</span>Settings</button>
+                <button><span className="material-icons-outlined">logout</span>Log out</button>
+            </div>
+        </div>
+
+        {/* Filter Options */}
+        <div className={`${styles.filterOptions} ${filterClicked && styles.filterActive}`}>
+            <button className={`${styles.activeFilterOption}`} ><span className="material-icons">home</span>Home</button>
+            <button><span className="material-icons-outlined">public</span>All</button>
+            <button><span className="material-icons-outlined">location_on</span>Local</button>
+        </div>
+
+        { /* Mobile Menu Overlay */}
+        <div onMouseUp={() => handleUserMenuOverlayClose()} onTouchEnd={() => handleUserMenuOverlayClose()} className={`${styles.overlay} z-50 ${userMenu && styles.overlayActive}`}></div>
+        
+        {/* Filter Overlay */}
+        <div onTouchEnd={() => handleFilterOverlayClose()} onMouseUp={() => handleFilterOverlayClose()} className={`${styles.overlay} z-10 ${filterClicked && styles.overlayActive}`}></div>
         </>
     )
 }
