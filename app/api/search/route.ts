c0c71031b21a617b2e3a83ba10cf5eb90f3a2c6a
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-12 01:33:04 +0200

    Added search bar

---
+++ app/api/search/route.ts
@@ -0,0 +1,41 @@
+import { LemmyHttp, ListingType, SortType, CommunityId, Search, SearchResponse, PersonId, SearchType } from "lemmy-js-client"
+
+// GET /api/search
+export async function GET(req: Request) {
+    try {
+        
+        let params = new URL(req.url).searchParams;
+
+        let q = params.get("q") || undefined;
+        let community_id = params.get("community_id") || "";
+        let community_name = params.get("community_name") || "";
+        let creator_id = params.get("creator_id") || "";
+        let type_ = params.get("type_") || "All";
+        let sort = params.get("sort") || "Hot";
+        let listing_type = params.get("listing_type") || "All";
+        let page = params.get("page") || 0;
+        let limit = params.get("limit") || 25;
+        let auth = params.get("auth") || "";
+
+        let client: LemmyHttp = new LemmyHttp("https://lemmy.world");
+        let searchResponse = await client.search({ 
+            q: q as unknown as string,
+            community_id: community_id as unknown as CommunityId,
+            community_name: community_name as unknown as string,
+            creator_id: creator_id as unknown as PersonId,
+            type_: type_ as unknown as SearchType,
+            sort: sort as unknown as SortType,
+            listing_type: listing_type as unknown as ListingType,
+
+            page: page as unknown as number,
+            limit: limit as unknown as number,
+            auth: auth as unknown as string
+        });
+
+        return new Response(JSON.stringify(searchResponse), { status: 200, headers: { 'Content-Type': 'application/json' } })
+
+    } catch (e: any) {
+        console.error("Search Error:",e);
+        return new Response(JSON.stringify({ error: e }), { status: 500, headers: { 'Content-Type': 'application/json' } })
+    }
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,9 @@ import { useNavbar, NavbarState } from "@/hooks/navbar";
 import styles from "../styles/Navbar.module.css";
 import { handleLogout } from "@/utils/authFunctions";
 import { useRouter } from "next/navigation"; 
-import { ListingType, SortType } from "lemmy-js-client";
+import { ListingType, Search, SearchResponse, SortType } from "lemmy-js-client";
+import Username from "./User/Username";
+import RenderMarkdown from "./ui/RenderMarkdown";
 
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
@@ -67,6 +69,26 @@ function FilterButton({ label, option, icon, navbar, setNavbar, setFilterClicked
     )
 }
 
+async function search({ searchParams } : { searchParams: Search }) {
+    console.log("searchParams", searchParams)
+
+    // add a signature to the object to make typescript happy
+    const params: { [index: string]: any } = {
+        ...searchParams
+    }
+    
+    // filter out undefined values
+    Object.keys(params).forEach(key => params[key] === undefined && delete params[key])
+
+    const query = Object.keys(params).map((key) => key + '=' + params[key]).join('&')
+    const requestUrl = `/api/search?${query}`
+
+    const response = await fetch(requestUrl)
+
+    const data = await response.json();
+    return data;
+}
+
 
 export default function Navbar() {
     const { session, setSession } = useSession();
@@ -76,12 +98,26 @@ export default function Navbar() {
     const [sortOptions, setSortOptions] = useState(false);
     const [userMenu, setUserMenu] = useState(false);
     const [menu, setMenu] = useState(false);
+    const [searchOverlay, setSearchOverlay] = useState(false);
+    const [currentSearch, setCurrentSearch] = useState("");
+    const [searchResults, setSearchResults] = useState<SearchResponse>({} as SearchResponse);
 
     const router = useRouter();
 
+    useEffect(() => {
+        console.log("Search:", currentSearch)
+    }, [currentSearch])
+
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        alert("This isn't doing anything yet.")
+        setIsSearching(true);
+        const resp = await search({ searchParams: { 
+            q: currentSearch, community_id: undefined, 
+            community_name: undefined, creator_id: undefined, type_: "Posts", sort: undefined, 
+            listing_type: undefined, page: undefined, limit: undefined, auth: session?.jwt || undefined 
+        }})
+        console.log(resp);
+        setSearchResults(resp);
     }
 
     const handleFilterOverlayClose = async () => {
@@ -120,6 +156,12 @@ export default function Navbar() {
         await delay(100);
 
         setMenu(false);
+    }
+
+    const handleCloseSearchOverlay = async () => {
+        setCurrentSearch("");
+        setIsSearching(false);
+        setSearchOverlay(false);
     }
 
     if(navbar?.hidden) return null;
@@ -168,23 +210,12 @@ export default function Navbar() {
                     }
                 </div>
 
-                
-
             </div>
-            
-
-            
-
-          
 
             <div className="flex flex-row items-center gap-4">
 
                 { navbar?.showSearch &&
-                <form onSubmit={(e) => handleSubmit(e)} className={`${styles.navButton} ${styles.searchWrapper} ${isSearching && styles.searchWrapperActive}`}>
-                    <span className="material-icons">search</span>
-                    
-                    {isSearching && <input onBlur={(e) => e.currentTarget.value.length == 0 && setIsSearching(false)} className={`${styles.search}`} type="text" placeholder="Search" />}
-                </form>
+                    <button className="flex justify-center items-center" onClick={() => setSearchOverlay(true)}><span className="material-symbols-outlined">search</span></button>
                 }
 
                 
@@ -208,6 +239,110 @@ export default function Navbar() {
             </div>
             
         </nav>
+
+        {/* Search Overlay */}
+        <div id="search" className={`${styles.searchOverlay} ${searchOverlay && styles.searchActive}`}>
+                <div className={`${styles.searchInputWrapper}`}>
+                    <button onClick={() => handleCloseSearchOverlay()} ><span className="material-symbols-outlined text-neutral-400">arrow_back</span></button>
+                    <form onSubmit={handleSubmit} className="flex flex-row items-center w-full">
+                        <div className={`${styles.searchInput}`}>
+                            <div className="flex flex-row gap-2 items-center w-full">
+                                <span className="material-symbols-outlined text-neutral-400 select-none">search</span>
+                                <input 
+                                    value={currentSearch} onChange={(e) => setCurrentSearch(e.currentTarget.value)} 
+                                    type="text" placeholder="Search" className="w-full h-full bg-transparent appearance-none outline-none font-bold" />
+                            </div>
+                           
+                            <button type="button" className="flex items-center justify-center" onClick={() => setCurrentSearch("")}><span className="material-symbols-outlined filled text-neutral-400 select-none">cancel</span></button>
+                        </div>
+                        
+                    </form>
+                </div>
+
+                {!isSearching &&
+                <div className={`${styles.searchOverlayTrending}`}>
+
+                    <div className="flex flex-col gap-2">
+                        <span className="font-bold text-xl">Popular</span>
+                        <div className="bg-neutral-950 p-4 flex flex-row justify-between rounded-xl border dark:border-fuchsia-800">
+                            <div className="flex flex-row gap-1 w-9/12">
+                                <span className="material-symbols-outlined" style={{ fontSize: "2rem" }}>chart_data</span>
+                                <div className="flex flex-col">
+                                    <span className="font-bold">Reddit Moment</span>
+                                    <span className="text-neutral-300">c/Memes@lemmy.ml</span>
+                                </div>
+                            </div>
+                            <img className="h-20 w-20 rounded-lg" src={"https://lemmy.world/pictrs/image/5d9409fd-a9a0-4303-bb7a-dd8f2799af73.png?format=webp"} alt="" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <span className="font-bold text-xl">Trending communities</span>
+                        <div className="bg-neutral-950 p-4 flex flex-row justify-start items-center gap-2 rounded-xl border dark:border-fuchsia-800">
+                            <img className="h-12 w-12 rounded-full" src={"https://lemmy.world/pictrs/image/5d9409fd-a9a0-4303-bb7a-dd8f2799af73.png?format=webp"} alt="" />
+                            <div className="flex flex-col gap-1">
+                                <span className="font-bold">Techno</span>
+                                <div className="flex flex-row gap-2 h-fit">
+                                    <div className="snack"><span className="material-symbols-outlined">communities</span>2k</div>
+                                    <div className="snack"><span className="material-symbols-outlined">group</span>2 / Day</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                }
+
+                { isSearching &&
+                <div className="flex flex-col gap-0 w-full overflow-scroll h-full relative">
+                    {searchResults.posts?.map((result, index) => (
+                        <div key={index} className="flex flex-row p-4 items-center justify-between dark:bg-neutral-950 border-b border-neutral-700">
+                            <div className="flex flex-col gap-3">
+
+                                <div className="flex flex-row items-center gap-2 ">
+                                    <img className="h-12 w-12 rounded-full" src={result?.community?.icon} alt="" />
+                                    <div className="flex flex-col gap-1">
+                                        <span>c/{result?.community?.name}</span>
+                                        <div className="flex flex-row">
+                                            {result.creator && <Username user={result.creator} baseUrl="" />}
+                                            <div className="divider-dot"></div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col gap-1">
+                                    <span className="font-bold">{result?.post?.name}</span>
+                                   {!result?.post?.thumbnail_url && <span className="text-neutral-300 line-clamp-2"><RenderMarkdown>{result?.post?.body}</RenderMarkdown></span>}
+                                </div>
+
+                                <div className="flex flex-row gap-4">
+                                    <div className="flex flex-row gap-2 items-center">
+                                        <div className="flex items-center gap-1">
+                                            <span className="material-symbols-outlined">thumb_up</span>
+                                            <span className="text-neutral-300">{result?.counts.upvotes}</span>
+                                        </div>
+                                        <div className="flex items-center gap-1">
+                                            <span className="material-symbols-outlined">thumb_down</span>
+                                            <span className="text-neutral-300">{result?.counts?.downvotes}</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex items-center gap-1">
+                                        <span className="material-symbols-outlined">chat_bubble</span>
+                                        <span className="text-neutral-300">{result?.counts?.comments}</span>
+                                    </div>
+                                    
+                                </div>
+
+                            </div>
+                            {result?.post?.thumbnail_url && <img className="w-24 h-24 rounded-lg" src={result.post.thumbnail_url} alt="" /> }
+                        </div>
+                    ))}
+                </div>
+                }
+
+        </div>
 
         {/* Mobile Menu Left Side */}
         <div id="menu" className={`${styles.menu} ${menu && styles.menuActive}`}>
