ca978c9a6a2c0b0a9ee912b83e6249eec4fe363a
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-12 02:00:05 +0200

    Added karma to smallUser

--- components/User/SmallUser.tsx
+++ components/User/SmallUser.tsx
@@ -2,15 +2,44 @@ import { Person } from "lemmy-js-client";
 
 
 import styles from "../../styles/User/SmallUser.module.css"
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import Link from "next/link";
+import { NumericFormat } from "react-number-format";
 
 export default function SmallUser({ user, userHover, setUserHover, style, opensToTop=false } : { user: Person, baseUrl: string, userHover: boolean, setUserHover: Function, style?: CSSProperties, opensToTop?: boolean }) {
+    const [userData, setUserData] = useState<Person>({} as Person);
+    const [karma, setKarma] = useState<number>(0);
+
     if(!user) throw new Error("Passed User to SmallUser is undefined");
 
     const actor_id = new URL(user.actor_id);
 
     const baseUrl = actor_id.hostname;
+
+    
+    const getUserData = async () => {
+        const data = await fetch(`/api/getUser?username=${user.name}&baseUrl=${baseUrl}`);
+        const json = (await data.json());
+        if(json.error) {
+            console.error(json.error)
+        }
+        setUserData(json as Person);
+
+        let post_score= json?.person_view?.counts?.post_score;
+        let comment_score = json?.person_view?.counts?.comment_score;
+        let post_count = json?.person_view?.counts?.post_count;
+        let comment_count = json?.person_view?.counts?.comment_count;
+        
+        let comment_amount = json?.comments?.length;
+        let post_amount = json?.posts?.length;
+    
+        let tmp = Math.ceil(((post_score*0.9 + comment_score*0.5) + (comment_amount*0.5 + post_amount*0.9)) / (post_count*0.75 + comment_count*0.25)*20)
+        setKarma(tmp);
+    }
+
+    useEffect(() => {
+        getUserData();
+    }, [user])
 
     return (
         <>
@@ -35,6 +64,7 @@ export default function SmallUser({ user, userHover, setUserHover, style, opensT
                         <span className={`${styles.name}`}><span className=" select-all ">@{user.name}</span> {!user.local && !user.display_name && `on ${baseUrl}`}</span>
                     </div>
                 </div>
+                {karma > 0 && <span className="snack"><NumericFormat displayType="text" className="flex bg-transparent w-full appearance-none " value={karma} thousandSeparator /> Points</span>}
                 <Link className="flex h-full" href={baseUrl == "lemmy.world" ? `/u/${user.name}` : `https://${baseUrl}/u/${user.name}`} target="_blank"><span className="material-icons">open_in_new</span></Link>
             </div>
         </>
