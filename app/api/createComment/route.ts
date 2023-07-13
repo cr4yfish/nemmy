6053dab908317455342568f5a58cf14d1deb4bef
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-13 02:03:21 +0200

    Added createComment API route

---
+++ app/api/createComment/route.ts
@@ -0,0 +1,31 @@
+import { LemmyHttp, PostId, CommentId } from "lemmy-js-client"
+
+export async function POST(req: Request) {
+    try {
+
+        const body = await req.json();
+
+        let post_id = body.post_id || undefined;
+        let content = body.content || undefined;
+        let auth = body.auth || undefined;
+        let language_id = body.language_id || undefined;
+        let form_id = body.form_id || undefined;
+        let parent_id = body.parent_id || undefined;
+
+        if(!content || !post_id || !auth) throw new Error("missing parameters");
+
+        let client: LemmyHttp = new LemmyHttp(`https://lemmy.world`);
+
+        let response = await client.createComment({
+            content: content as unknown as string,
+            post_id: post_id as unknown as PostId,
+            auth: auth as unknown as string,
+        })
+
+        return new Response(JSON.stringify(response), { status: 200, headers: { 'Content-Type': 'application/json' } })
+
+    } catch (e: any) {
+        console.error("createComment Error:",e);
+        return new Response(JSON.stringify({ error: e }), { status: 500, headers: { 'Content-Type': 'application/json' } })
+    }
+}
