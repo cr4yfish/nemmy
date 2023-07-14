327280482395285c031175e06bd3cf93dbf50ff3
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-14 02:20:22 +0200

    Added send comment function

--- utils/lemmy.ts
+++ utils/lemmy.ts
@@ -1,5 +1,6 @@
 import { deleteCookie } from "cookies-next";
-import { CreatePost, GetSiteResponse, ListCommunities, ListCommunitiesResponse, PostResponse } from "lemmy-js-client"
+import { CommentResponse, CommentView, CreateComment, CreatePost, GetComments, GetCommentsResponse, GetPostResponse, GetSiteResponse, ListCommunities, ListCommunitiesResponse, PostResponse } from "lemmy-js-client"
+import { FormEvent } from "react";
 
 export const getUserDetails = async (jwt: string) :  Promise<(boolean | GetSiteResponse)> => {
     const user: GetSiteResponse = await fetch(`/api/getSite?auth=${jwt}`).then(res => res.json());
@@ -34,4 +35,26 @@ export const createPost = async (params: CreatePost) : Promise<(boolean | PostRe
         return false;
     }
     return response as PostResponse;
+}
+
+export const sendComment = async (params: CreateComment) => {
+    const data: CommentResponse = await fetch(`/api/createComment`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ content: params.content, auth: params.auth, post_id: params.post_id, parent_id: params.parent_id })
+    }).then(res => res.json());
+
+    if(!data?.comment_view?.comment?.id) return alert("Something went wrong sending the comment");
+
+    return data.comment_view;
+}
+
+export const getComments = async (params: GetComments, baseUrl: string) : Promise<(void | GetCommentsResponse)> => {
+    const data = await fetch(`/api/getComments?post_id=${params.post_id}&sort=${params.sort}&limit=${params.limit}&page=${params.page}&max_depth=${params.max_depth}&baseUrl=${baseUrl}&type_=All&auth=${params.auth}`).then(res => res.json());
+    if(!data?.comments) {
+        return console.warn("Something went wrong getting the comments");
+    }
+    return data;
 }
