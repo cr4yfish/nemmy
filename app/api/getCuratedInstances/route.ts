78f8a6c38a87630e641f0ad08f59a1a8390a0d58
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-15 11:35:45 +0200

    Removed some whitespace

--- app/api/getCuratedInstances/route.ts
+++ app/api/getCuratedInstances/route.ts
@@ -5,7 +5,6 @@ export async function GET(req: Request) {
         const url_to_csv = "https://raw.githubusercontent.com/maltfield/awesome-lemmy-instances/main/awesome-lemmy-instances.csv";
 
         const data = parse(await (await fetch(url_to_csv)).text(), { header: true }).data;
-        
 
         return new Response(JSON.stringify(data), { status: 200, headers: { 'Content-Type': 'application/json' } })
 
