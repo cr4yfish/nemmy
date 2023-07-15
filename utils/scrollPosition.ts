dc143dbdf413aed27e2434e6381aeccab0c89691
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-15 14:51:53 +0200

    Added scroll position functions

---
+++ utils/scrollPosition.ts
@@ -0,0 +1,26 @@
+export const getStorage = () => sessionStorage
+
+export const saveScrollPos = (asPath: string) => {
+  try {
+    getStorage().setItem(`scrollPos:${asPath}`, JSON.stringify({ x: window.scrollX, y: window.scrollY }))
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
+}
+
+export const restoreScrollPos = (asPath: string) => {
+  try {
+    const json = getStorage().getItem(`scrollPos:${asPath}`)
+    const scrollPos = json ? JSON.parse(json) : undefined
+    if (scrollPos && scrollPos.y) {
+      window.scrollTo(scrollPos.x, scrollPos.y)
+    }
+    // eslint-disable-next-line no-empty
+  } catch (e) {}
+}
+
+export const deleteScrollPos = (asPath: string) => {
+  try {
+    getStorage().removeItem(`scrollPos:${asPath}`)
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
+}
