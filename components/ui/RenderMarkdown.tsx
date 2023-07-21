6e95644c78e85752c6552f7f5d4135b042a1cad2
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-21 19:07:27 +0200

    Improved markdown rendering

--- components/ui/RenderMarkdown.tsx
+++ components/ui/RenderMarkdown.tsx
@@ -2,10 +2,10 @@ import ReactMarkdown from "react-markdown"
 
 import styles from "@/styles/util/markdown.module.css"
 
-export default function RenderMarkdown({ children } : { children: React.ReactNode }) {
+export default function RenderMarkdown({ children, content } : { children?: React.ReactNode, content?: React.ReactNode }) {
     return (
         <ReactMarkdown className={`${styles.markdown}`}>
-            {`${children}`}
+            {`${children || content}`}
         </ReactMarkdown>
     )
 }
