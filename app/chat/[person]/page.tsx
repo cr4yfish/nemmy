2e7cb11675ed63df1382c3f9dd888ecbd9aeb04e
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-21 19:08:27 +0200

    Added chat

---
+++ app/chat/[person]/page.tsx
@@ -0,0 +1,106 @@
+import { LemmyHttp, PrivateMessageView, PrivateMessagesResponse } from "lemmy-js-client"
+import { cookies } from "next/dist/client/components/headers";
+import Link from "next/link";
+
+import RenderMarkdown from "@/components/ui/RenderMarkdown";
+import { DEFAULT_INSTANCE, DEFAULT_AVATAR } from "@/constants/settings";
+import RenderError from "@/components/ui/RenderError";
+
+import styles from "@/styles/chat.module.css";
+
+// Returns all messages from a specific user, sorted by newest message
+async function getMessages({ auth, instance, creator} : { auth: string, instance: string, creator: string }): Promise<PrivateMessageView[]> {
+    const client = new LemmyHttp(instance ? `https://${instance}` : DEFAULT_INSTANCE);
+    const messages = await client.getPrivateMessages({ 
+        auth: auth, 
+        page: 1,
+        unread_only: false,
+    });
+
+    // Get all messages from and to the creator
+    const filtered = messages.private_messages.filter((m) => (m.creator.name == creator) || (m.recipient.name == creator));
+
+    // sort by newest message
+    filtered.sort((a, b) => {
+        return new Date(b.private_message.published).getTime() - new Date(a.private_message.published).getTime();
+    }).reverse();
+
+    return filtered;
+}
+
+function addZero(n: number) {
+    return n < 10 ? `0${n}` : n;
+}
+
+function getDayMonth(date: string) {
+    const d = new Date(date);
+    return `${addZero(d.getDate())}.${addZero(d.getMonth() + 1)}`
+}
+
+function getHoursMinutes(date: string) {
+    const d = new Date(date);
+    return `${addZero(d.getHours())}:${addZero(d.getMinutes())}`
+}
+
+function RenderDate({ date } : { date: string }) {
+
+    return (
+        <>
+        <span className="text-xs text-neutral-600 italic bg-neutral-700/10 p-1 px-2 rounded-full">{date}</span>
+        </>
+    )
+}
+
+export default async function UserChat({ params: { person } } : { params: { person: string } }) {
+    try {
+        const cookiesStore = cookies();
+        const auth = cookiesStore.get("jwt")?.value, instance = cookiesStore.get("instance")?.value;
+        if(!auth || !instance) return (
+            <>
+            <div className="flex flex-col gap-1 justify-center items-center w-full">
+                <h1>Not logged in</h1>
+                <Link href={"/auth"}><button className="a">Log in</button></Link>
+            </div>
+            </>
+        );
+
+        const messages = await getMessages({ auth: auth, instance: instance, creator: person });
+
+        console.log("Person:", person)
+
+        let currentDate = getDayMonth(messages[0].private_message.published);
+
+        return (
+            <>
+            <div className="flex flex-col gap-4 items-center justify-end max-sm:px-4 w-full px-14 max-sm:max-w-3xl pb-48" >
+                <RenderDate date={currentDate} />
+                {messages.map((m) => {
+                    const date = getDayMonth(m.private_message.published);
+                    const isSameDate = date == currentDate;
+                    if(!isSameDate) currentDate = date;
+
+                    return (
+                        <>
+                        {!isSameDate && <RenderDate date={currentDate} />}
+                        <div className={` bg-neutral-200 text-neutral-950 rounded-3xl p-4 pb-6 pr-6 relative max-w-sm self-start overflow-ellipsis overflow-clip ${m.creator.name != person && styles.ownMessage}`}>
+                            <span className={`absolute right-3 bottom-2 text-neutral-600 ${m.creator.name != person && styles.ownMessageMetadata} font-light italic text-xs`}>{getHoursMinutes(m.private_message.published)}</span>
+                            <RenderMarkdown key={m.private_message.id}>{m.private_message.content}</RenderMarkdown>
+                        </div>
+                        </>
+                    )
+                })}
+
+            </div>
+            <div className="fixed bottom-0 left-0 h-20 w-full bg-neutral-50 border-t border-neutral-400 flex items-center">
+                <input className="bg-transparent w-full h-full p-4" type="text" name="" id="" />
+                <button className=" h-full bg-neutral-50 flex items-center z-10 w-10"><span className="material-symbols-outlined absolute right-5">send</span></button>
+            </div>
+            </>
+        )
+    } catch(e) {
+        console.log(e)
+        return (
+            <RenderError />
+        )
+    }
+}
---
+++ app/chat/page.tsx
@@ -0,0 +1,96 @@
+import { DEFAULT_AVATAR, DEFAULT_INSTANCE } from "@/constants/settings";
+import { GetPrivateMessages, LemmyHttp, PrivateMessagesResponse } from "lemmy-js-client";
+import { cookies } from "next/dist/client/components/headers";
+import Link from "next/link";
+
+import { FormatDate } from "@/utils/formatDate";
+import RenderMarkdown from "@/components/ui/RenderMarkdown";
+import RenderError from "@/components/ui/RenderError";
+
+async function getChats({ auth, instance} : { auth: string, instance: string }): Promise<PrivateMessagesResponse> {
+    const client = new LemmyHttp(instance ? `https://${instance}` : DEFAULT_INSTANCE);
+    const chats = await client.getPrivateMessages({ 
+        auth: auth, 
+        page: 1,
+        unread_only: false,
+    });
+    return chats;
+}
+
+function ChatPreview({creator, chats} : {creator: any, chats: PrivateMessagesResponse}) {
+    const lastMessage = chats.private_messages.filter((m) => m.creator.id == creator.id)[0].private_message;
+    return (
+        <>
+        <Link href={`/chat/${creator.name}`} key={creator.id} className="flex flex-row items-center gap-2 w-full bg-fuchsia-100 p-4 rounded-lg cursor-pointer">
+            <div className=" h-20 w-20 overflow-hidden rounded-full flex items-center justify-center">
+                <img src={creator.avatar || DEFAULT_AVATAR} alt="" className="w-full h-full object-contain"  />
+            </div>
+            <div className="flex flex-col w-full">
+                <div className=" w-full flex justify-between">
+                    <span className="font-xl font-bold text-fuchsia-950">{creator.name}</span>
+                    <span className=" text-xs font-light text-fuchsia-600">{FormatDate({date: new Date(lastMessage.published)})}</span>
+                </div>
+                
+                {/* newest message by creator */}
+                <div className=" text-ellipsis overflow-hidden overflow-clip line-clamp-1 font-light text-fuchsia-900 max-w-xs">
+                    <RenderMarkdown>{lastMessage.content}</RenderMarkdown>
+                </div>
+            </div>
+        </Link>
+        </>
+    )
+}
+
+
+export default async function Chat() {
+    try {
+        const cookiesStore = cookies();
+        const auth = cookiesStore.get("jwt")?.value, 
+            instance = cookiesStore.get("instance")?.value;
+        
+        if(!auth || !instance) return (
+            <>
+            <div className="flex flex-col gap-1 justify-center items-center w-full">
+                <h1>Not logged in</h1>
+                <Link href={"/auth"}><button className="a">Log in</button></Link>
+            </div>
+            </>
+        );
+
+        let chats = await getChats({ auth: auth, instance: instance });
+        const recipient = chats.private_messages[0].recipient;
+
+        // filter out all messages that are from the recipient
+        chats.private_messages = chats.private_messages.filter((chat) => chat.creator.id != recipient.id);
+
+        // sort chats by newest message
+        chats.private_messages.sort((a, b) => {
+            const aDate = new Date(a.private_message.published);
+            const bDate = new Date(b.private_message.published);
+            return bDate.getTime() - aDate.getTime();
+        });
+
+        // get all creators in an array (with duplicates)
+        const creators = chats.private_messages.map((chat) => chat.creator);
+
+        // remove duplicates
+        const uniqueCreators = creators.filter((creator, index) => {
+            return creators.findIndex((c) => c.id == creator.id) == index;
+        });
+
+        return (
+            <>
+            <div className="w-full flex flex-col gap-2 max-w-3xl max-sm:px-4">
+                {uniqueCreators.map((creator) => (
+                    <ChatPreview key={creator.id} creator={creator} chats={chats} />
+                ))}
+            </div>
+            </>
+        )
+    } catch(e) {
+        console.error(e);
+        return (
+            <RenderError />
+        )
+    }
+}
