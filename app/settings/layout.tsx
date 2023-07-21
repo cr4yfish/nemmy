d4215c88a45cecf9cae5d4e16c60d2d726e9e1ec
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-21 19:09:52 +0200

    Added settings blockout

---
+++ app/settings/layout.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect } from "react";
+import { useNavbar } from "@/hooks/navbar"
+
+export default function Layout({
+    children 
+} : { children: React.ReactNode}) {
+
+    const { navbar, setNavbar } = useNavbar();
+
+    useEffect(() => {
+        setNavbar({ ...navbar!, 
+            showSort: false, showFilter: false, 
+            showSearch: false, showback: false, 
+            hidden: false,
+            titleOverride: "Settings", icon: "page_info",
+            showUser: true
+        })
+    }, [])
+
+    return (
+        <div className="mt-24 min-h-screen w-full flex flex-col, justify-center py-4 mb-6">
+            {children}
+        </div>
+    )
+}
---
+++ app/settings/page.tsx
@@ -0,0 +1,59 @@
+import { GetSiteResponse, LemmyHttp } from "lemmy-js-client";
+import { cookies } from "next/dist/client/components/headers";
+
+import { DEFAULT_INSTANCE } from "@/constants/settings";
+
+function Section({ children, title }: { children: any, title: string }) {
+
+    return (
+        <>
+        <div className=" w-full" >
+            <span className=" w-full">{title}</span>
+            <div className="flex flex-col w-full bg-fuchsia-200 dark:bg-neutral-800 rounded-lg p-6">{children}</div>
+        </div>
+        </>
+    )
+}
+
+async function getUserData (username: string, jwt?: string, instance?: string): Promise<GetSiteResponse> {
+    const client: LemmyHttp = new LemmyHttp(instance ? `https://${instance}` : DEFAULT_INSTANCE);
+
+    const user = await client.getSite({
+        auth: jwt as unknown as string,
+    })
+    return user;
+}
+
+export default async function Settings() {
+    const cookieStore = cookies();
+    const jwt = cookieStore.get("jwt")?.value, instance = cookieStore.get("instance")?.value;
+
+    const userData = await getUserData("username", jwt, instance);
+
+    return (
+        <>
+        <div className="h-full max-w-3xl flex flex-col overflow-y-hidden">
+            <Section title="Default instance">
+                
+            </Section>
+            <Section title="Appearance">
+                <div className="flex flex-col gap-6">
+                    <div className="flex flex-row justify-between w-full">
+                        <span>Modern</span>
+                        <span>Compact</span>
+                    </div>
+                    <div>Use system theme</div>
+                </div>
+            </Section>
+
+            <Section title="Textsize">
+                <div className="flex flex-col gap-4">
+                    <div>Small</div>
+                    <div>Regular</div>
+                    <div>Big</div>
+                </div>
+            </Section>
+        </div>
+        </>
+    )
+}
