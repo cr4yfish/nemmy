0b37c08a7b6407255e0124b2c86e654a72aeb162
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-24 03:15:43 +0200

    Removed unused and wrong cookie usage

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    let jwt = req.cookies.get("jwt");
-
-    if(jwt?.value.length == 0) {
-        req.cookies.delete("jwt");
-    }
-
     const response = NextResponse.next();
 
     return response;
