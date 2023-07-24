61e21ae50c9ed7d1fa67be02ecd8b8b41016a965
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-24 17:11:10 +0200

    Added new component for scrolling ends

---
+++ components/ui/EndlessSrollingEnd.tsx
@@ -0,0 +1,13 @@
+
+
+export default function EndlessScrollingEnd() {
+
+    return(
+        <>
+        <div key={"scrollerend"} className=" w-full flex flex-col items-center justify-center gap-2 mt-24 mb-24">
+            <span className="material-symbols-outlined text-blue-400">bedtime</span>
+            <span>Nothing more to explore here</span>
+        </div>
+        </>
+    )
+}
