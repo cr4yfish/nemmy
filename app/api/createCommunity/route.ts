1d969dbf6af3ae04bc7a3d1ca9b207847e7d357c
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-26 11:39:19 +0200

    Added createCommunity Route

---
+++ app/api/createCommunity/route.ts
@@ -0,0 +1,42 @@
+import { LemmyHttp } from "lemmy-js-client"
+import { DEFAULT_INSTANCE } from "@/constants/settings";
+
+export async function POST(req: Request) {
+    try {
+
+        const body = await req.json();
+
+        let name = body.name || undefined;
+        let title = body.title || undefined;
+        let description = body.description || undefined;
+        let icon = body.icon || undefined;
+        let banner = body.banner || undefined;
+        let nsfw = body.nsfw || undefined;
+        let posting_restricted_to_mods = body.posting_restricted_to_mods || undefined;  
+        let discussion_languages = body.discussion_languages || undefined;
+        let auth = body.auth || undefined;
+        let instance = body.instance || undefined;
+
+        if(!name || !title || !auth) throw new Error("missing required parameters");
+
+        let client: LemmyHttp = new LemmyHttp(instance ? `https://${instance}` : DEFAULT_INSTANCE);
+
+        let response = await client.createCommunity({
+            name: name,
+            title: title,
+            description: description,
+            icon: icon,
+            banner: banner,
+            nsfw: nsfw,
+            posting_restricted_to_mods: posting_restricted_to_mods,
+            discussion_languages: discussion_languages,
+            auth: auth
+        })
+
+        return new Response(JSON.stringify(response), { status: 200, headers: { 'Content-Type': 'application/json' } })
+
+    } catch (e: any) {
+        console.error("createCommunity Error:",e);
+        return new Response(JSON.stringify({ error: e }), { status: 500, headers: { 'Content-Type': 'application/json' } })
+    }
+}
