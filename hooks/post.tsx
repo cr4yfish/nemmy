a0da18cc595f38ad64b64888adf70b80648acbbf
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-07-26 12:24:41 +0200

    Removed conosle log

--- hooks/post.tsx
+++ hooks/post.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { PostView } from 'lemmy-js-client';
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 interface PostContextProps {
     post: PostView | undefined;
@@ -13,10 +13,6 @@ const PostContext = createContext<PostContextProps>({ post: undefined, setPost: 
 export const PostContextProvider = ({ children } : { children: any }) => {
 
     const [post, setPost] = useState<PostView | undefined>(undefined);
-
-    useEffect(() => {
-        console.log("PostContextProvider: ", post)
-    }, [post])
 
     return (
         <PostContext.Provider value={{ post, setPost }}>
