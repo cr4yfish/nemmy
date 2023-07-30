bc490fbf6153712898c37ce0496bf4bcb146c15a
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-30 02:07:04 +0200

    Improved Markdown rendering

--- components/ui/RenderMarkdown.tsx
+++ components/ui/RenderMarkdown.tsx
@@ -1,20 +1,40 @@
 import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm"
 
 import styles from "@/styles/util/markdown.module.css"
 
 export default function RenderMarkdown({ 
-    children, content, className } : { 
-        children?: React.ReactNode, 
+    content, className } : { 
         content?: React.ReactNode, 
         className?: string 
     }) {
 
     return (
         <>
-        { children || content &&
-            <ReactMarkdown className={`${styles.markdown} ${className}`}>
-            {`${children || content}`}
-        </ReactMarkdown>
+        { content &&
+            <ReactMarkdown 
+                className={`${styles.markdown} ${className}`}
+                remarkPlugins={[remarkGfm]}
+                remarkRehypeOptions={{
+                    allowDangerousHtml: true,
+                    clobberPrefix: "content",
+                    handlers: {
+                        a: (h, node) => {
+                            return h(node, "a", {
+                                href: node.properties.href,
+                                target: "_blank",
+                                rel: "noopener noreferrer"
+                            }, node.children)
+                        }
+                    }
+                }}
+                components={{
+                    
+                }}
+                
+                >
+                {`${content}`}
+            </ReactMarkdown>
         }
         </>
     )
