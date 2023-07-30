32be2281aa92835c22cf14759d518e5ca5a7135e
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-30 23:13:09 +0200

    Improved Loader design

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -2,7 +2,7 @@ import styles from "../../styles/ui/Loader.module.css";
 
 export default function Loader() {
   return (
-    <div className={`${styles.loader} mt-4`} key={"loader"}>
+    <div className={`card ${styles.loader} mt-4`} key={"loader"}>
       <div className="h-20 w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800 max-md:hidden"></div>
       <div className="flex h-full w-full flex-col gap-4">
         <div className="h-4 w-full rounded-lg bg-neutral-200 dark:bg-neutral-800"></div>
