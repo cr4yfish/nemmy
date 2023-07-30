960a250dacdcca81bc2002a08aa198d173146f70
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-30 19:35:06 +0200

    Fixed defaults

--- app/api/listCommunities/route.ts
+++ app/api/listCommunities/route.ts
@@ -7,9 +7,9 @@ export async function GET(req: Request) {
   try {
     let params = new URL(req.url).searchParams;
 
-    let type_ = params.get("type_") || undefined;
-    let sort = params.get("sort") || undefined;
-    let page = params.get("page") || undefined;
+    let type_ = params.get("type_") || "All";
+    let sort = params.get("sort") || "Active";
+    let page = params.get("page") || 1;
     let limit = params.get("limit") || undefined;
     let auth = params.get("auth") || undefined;
     let instance = params.get("instance") || undefined;
