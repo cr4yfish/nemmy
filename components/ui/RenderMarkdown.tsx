45e0299b97f2a1582adbcfb3ccbd251e44fce106
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-07-30 17:51:30 +0200

    Added disable links mode for server rendering

--- components/ui/RenderMarkdown.tsx
+++ components/ui/RenderMarkdown.tsx
@@ -1,14 +1,17 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import Link from "next/link";
 
 import styles from "@/styles/util/markdown.module.css";
 
 export default function RenderMarkdown({
   content,
   className,
+  disableLinks = false,
 }: {
   content?: React.ReactNode;
   className?: string;
+  disableLinks?: boolean;
 }) {
   return (
     <>
@@ -34,7 +37,12 @@ export default function RenderMarkdown({
               },
             },
           }}
-          components={{}}
+          components={{
+            a ({ node, children, href, ...props }) {
+              if(!href) return <span className="a">{children}</span>
+              return disableLinks ? (<span className="a">{children}</span>) : (<Link rel="noopener noreferrer" href={href} {...props}>{children}</Link>);
+            }
+          }}
         >
           {`${content}`}
         </ReactMarkdown>
