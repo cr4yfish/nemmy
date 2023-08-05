03de8b4c068b972409cae876f521580de97fef1e
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-08-06 00:20:20 +0200

    Fixed loader style

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -1,8 +1,25 @@
-import styles from "../../styles/ui/Loader.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Loader() {
   return (
-    <div className={`card ${styles.loader} mt-4`} key={"loader"}>
+    <motion.div 
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+      className={` 
+        flex h-44 w-full
+        animate-pulse flex-row
+        items-start gap-5
+        rounded-lg
+        bg-neutral-50 p-4
+        shadow-md
+        dark:bg-neutral-900
+        dark:shadow-md
+        dark:border-neutral-700
+      `} 
+      style={{ width: "640px" }}
+      key={"loader"}
+    >
       <div className="h-20 w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800 max-md:hidden"></div>
       <div className="flex h-full w-full flex-col gap-4">
         <div className="h-4 w-full rounded-lg bg-neutral-200 dark:bg-neutral-800"></div>
@@ -14,6 +31,6 @@ export default function Loader() {
           <div className="h-full w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800"></div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
