646d562f8494b69bc66697f0b3af609b7ed33c37
Manuel <58395553+cr4yfish@users.noreply.github.com>
2023-08-09 12:35:02 +0200

    Added middleware check to remove old system

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,27 @@
+import { cookies } from "next/dist/client/components/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const response = NextResponse.next();
 
+  const accounts = req.cookies.get("accounts");
+  const currentAccount = req.cookies.get("currentAccount");
+  const defaultAccount = req.cookies.get("defaultAccount");
+
+  // Wipe old system
+  if (accounts?.value && currentAccount?.value && defaultAccount?.value) {
+    if(JSON.parse(currentAccount.value || "").hasOwnProperty("jwt")) {
+      console.log("Wiping old system")
+      response.cookies.delete("accounts");
+      response.cookies.delete("currentAccount");
+      response.cookies.delete("defaultAccount");
+    }
+  }
+  
+
   return response;
 }
 
 export const config = {
-  matcher: "/api/:path*",
+  matcher: "/",
 };
